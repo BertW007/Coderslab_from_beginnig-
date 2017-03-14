d37b8602f88268107b4f60895b3b4ef2d6835dc4
Wiktor Ramut <w.ramut@o2.pl>
2017-03-14 16:37:45 +0100

    OOP static function  done

--- OOP/AdvancedCalculator.php
+++ OOP/AdvancedCalculator.php
@@ -3,11 +3,17 @@ include_once 'Calculator.php';
 
 class AdvancedCalculator extends Calculator
 {
+    static private $pi = 3.14;
+
     public function power(){
         $this->array['power'] = pow($this->num1,$this->num2);
     }
     public function root(){
         $this->array['root'] = pow($this->num2,$this->num1);
+    }
+    static public function computeCircleRadius($r)
+    {
+        return self::$pi*pow($r,2);
     }
 }
 
@@ -19,3 +25,4 @@ $calc->divide();
 $calc->power();
 $calc->root();
 $calc->getArray();
+echo AdvancedCalculator::computeCircleRadius(4);
